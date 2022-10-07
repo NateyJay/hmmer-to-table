af50ce07af3430a94cdb0d45a0ac6c154118ab30
Nate Johnson <jax523@gmail.com>
2022-10-07 13:35:13 -0300

    Update plot_gene_model.R

--- plot_gene_model.R
+++ plot_gene_model.R
@@ -8,7 +8,7 @@ hmm.df <- read.delim("/Users/jax/Google Drive/++github/hmmer-to-table/test_files
 head(hmm.df)
 
 
-protein <- unique(hmm.df$target_accession)[2]
+protein <- unique(hmm.df$target_accession)[3]
 
 
 df <- hmm.df[hmm.df$target_accession == protein,]
@@ -25,15 +25,15 @@ box.h = 0.1
 
 
 
-plot(0,0,type='n', xlim=c(0, max(df$env_to)), ylim=c(0, nrow(df)+1), xlab='', ylab='', axes=F)
-abline(v=axisTicks(c(0, max(df$env_to)), F), col=scales::alpha('red',.5), lty=3)
+plot(0,0,type='n', xlim=c(0, max(df$ali_to)), ylim=c(0, nrow(df)+1), xlab='', ylab='', axes=F)
+abline(v=axisTicks(c(0, max(df$ali_to)), F), col=scales::alpha('red',.5), lty=3)
 axis(3)
 
-segments(0, nrow(df)+1, max(df$env_to), nrow(df)+1, lwd=2)
-rect(df$env_from, nrow(df)+box.h+1, df$env_to,  nrow(df)-box.h+1, col=df$col)
+segments(0, nrow(df)+1, max(df$ali_to), nrow(df)+1, lwd=2)
+rect(df$ali_from, nrow(df)+box.h+1, df$ali_to,  nrow(df)-box.h+1, col=df$col)
 
-rect(df$env_from, df$y+box.h, df$env_to,  df$y-box.h, col=df$col)
-text(df$env_from, df$y, df$query, pos=2)
+rect(df$ali_from, df$y+box.h, df$ali_to,  df$y-box.h, col=df$col)
+text(df$ali_from, df$y, df$query, pos=2)
 
 
 
